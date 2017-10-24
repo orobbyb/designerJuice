8ed4e7afebb49cf718c2114fb567517b7a469e49
jposner109 <jacob.posner109@gmail.com>
2017-10-24 11:30:07 -0500

    Hey it moves!

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,28 @@
-function replace(x) {
-    var fruit = document.getElementById(x);
+    var xpos = 550;
+    var ypos = 100;
+    var change = 0;
+
+function replace(b) {
+    xpos = xpos + 50;
+    change = change + 1;
+    if(change % 4 == 0){
+        ypos = ypos + 50;
+        xpos = 100;
+    }
+    var name="newImg"+change;
+    var x = xpos.toString();
+    var y = ypos.toString();
+    var fruit = document.getElementById(b);
     var place = fruit.src;
-    var newImg = document.createElement("img");
-    newImg.setAttribute("src", place);
-    newImg.setAttribute("height", fruit.height / 2);
-    newImg.setAttribute("width", fruit.width / 2);
-    newImg.style.position="absolute";
+    name = document.createElement("img");
+    name.setAttribute("src", place);
+    name.setAttribute("height", fruit.height / 2);
+    name.setAttribute("width", fruit.width / 2);
+    name.style.position="relative"; 
+    
     var wrapper = document.getElementById("bwrapper");
-    wrapper.appendChild(newImg);
-    wrapper.insertBefore(document.getElementById("blender"), newImg);
+    wrapper.appendChild(name);
+    name.style.marginLeft = x;
+    name.style.marginRight = y;
+//    wrapper.insertBefore(document.getElementById("blender"), newImg);
 }
