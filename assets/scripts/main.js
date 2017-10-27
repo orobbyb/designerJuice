f3a1ecd7fad1623d8164e759f8fcc03046c3b470
jposner109 <jacob.posner109@gmail.com>
2017-10-27 11:32:13 -0500

    sort of works blend button

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,9 +11,52 @@ function show(b){
     fruit.style.visibility = "visible";
     if(b == "grapes1"){
         grapesCounter = grapesCounter + 1;
-        var grapeString = "Grapes: "+grapesCounter;
-        document.getElementById("grapeOrder").innerHTML = grapeString;
+        var aString = "Grapes: "+grapesCounter;
+        document.getElementById("grapeOrder").innerHTML = aString;
     }
+    if(b == "apple1"){
+        appleCounter = appleCounter + 1;
+        var aString = "Apple: "+appleCounter;
+        document.getElementById("appleOrder").innerHTML = aString;
+    }
+    if(b == "strawberry1"){
+        strawberryCounter = strawberryCounter + 1;
+        var aString = "Strawberry: "+strawberryCounter;
+        document.getElementById("strawberryOrder").innerHTML = aString;
+    }
+    if(b == "cherries1"){
+        cherriesCounter = cherriesCounter + 1;
+        var aString = "Cherries: "+cherriesCounter;
+        document.getElementById("cherriesOrder").innerHTML = aString;
+    }
+    if(b == "orange1"){
+        orangeCounter = orangeCounter + 1;
+        var aString = "Orange: "+orangeCounter;
+        document.getElementById("orangeOrder").innerHTML = aString;
+    }
+    if(b == "banana1"){
+        bananaCounter = bananaCounter + 1;
+        var aString = "Banana: "+bananaCounter;
+        document.getElementById("bananaOrder").innerHTML = aString;
+    }
+    if(b == "carrot1"){
+        carrotCounter = carrotCounter + 1;
+        var aString = "Carrot: "+carrotCounter;
+        document.getElementById("carrotOrder").innerHTML = aString;
+    }
+}
+
+function blend(){
+    document.getElementsByClassName("fruit").style.visibility = "hidden";
+    var grapesColor = rgb(100, 50, 115);
+    var appleColor = rgb(219, 23, 35);
+    var strawberryColor = rgb(230, 29, 33);
+    var cherriesColor = rgb(187, 23, 32);
+    var orangeColor = rgb(235, 110, 35);
+    var bananaColor = rgb(254, 213, 49);
+    var carrotColor = rgb(246, 152, 48);
+    var blendedColor = (grapesColor * grapesCounter + appleColor * appleCounter + strawberryColor * strawberryCounter + cherriesColor * cherriesCounter + orangeColor * orangeCounter + bananaColor * bananaCounter + carrotColor * carrotCounter) / (grapesCounter + appleCounter + strawberryCounter + cherriesCounter + orangeCounter + bananaCounter + carrotCounter);
+    document.getElementByTagName(body).style.background-color = blenderColor;
 }
 
 //    var xpos = 550;
